c968898f38ee4ec40c019b05f61048585bffaf25
SiriusBrightstar <siriusbrightstar@protonmail.com>
2021-06-04 19:24:03 +0530

    Added welp() to print all the present functions

--- getnse.py
+++ getnse.py
@@ -3,6 +3,14 @@ import yaml
 
 nse = Nse()
 
+def welp():
+    print('clear():         Clears Screen')
+    print('nse_clr():       Clears Data')
+    print('EQ(\'Ticker\'):  Prints Stock Info')
+    print('MM():            Prints FII & DII data. MM: Market Manipulators')
+    print('stonk(n):        Prints n Top Gainers (F&O Only)')
+    print('stink(n):        Prints n Top Losers (F&O Only)')
+
 def clear():
     os.system('clear')
 
@@ -13,11 +21,11 @@ def EQ(Stk):
     x = nse.get_quote(Stk)
     print(yaml.dump(x, sort_keys=False, default_flow_style=False))
 
-def FD():
+def MM():
     print(nse.fii_dii().T)
 
-def gainers(x=10):
+def stonk(x=10):
     print(nse.top_gainers(index=IndexSymbol.FnO, length=x))
 
-def losers(x=10):
+def stink(x=10):
     print(nse.top_losers(index=IndexSymbol.FnO, length=x))
